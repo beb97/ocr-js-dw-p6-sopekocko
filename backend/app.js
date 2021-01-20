509b7e9239e6279ae8b081dca798f6c600fc8eb5
beb97 <beb97@hotmail.com>
2021-01-20 18:08:42 +0100

    pretify du app.js

--- backend/app.js
+++ backend/app.js
@@ -2,42 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const rateLimit = require('express-rate-limit');
-require('dotenv').config();
-
-var cors = require('cors');
-
 const authRoutes = require('./routes/auth');
 const sauceRoutes = require('./routes/sauce');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use('/images/', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth/', authRoutes);
+app.use('/api/sauces/', sauceRoutes);
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100}));
+app.use(cors({origin: 'http://localhost:4200'}));
 
 mongoose.connect(
     process.env.DB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
+    { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
     console.log('Successfully connected to MongoDB Atlas!');
-  })
-  .catch((error) => {
+})
+.catch((error) => {
     console.log('Unable to connect to MongoDB Atlas!');
     console.error(error);
-  });
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // = 15 minutes
-    max: 100
-})
-
-app.use(limiter);
-
-app.use(cors({origin: 'http://localhost:4200'}));
-
-app.use('/images/', express.static(path.join(__dirname, 'images')));
-app.use(bodyParser.json());
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/api/auth/', authRoutes);
-app.use('/api/sauces/', sauceRoutes);
+});
 
 module.exports = app;
